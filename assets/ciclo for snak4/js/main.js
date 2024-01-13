50c18125c72c34efbe8ee3b829fcd756af1c1339
federicoverbicaro <federico.verbicaro@icloud.com>
2024-01-13 17:07:34 +0100

    feat: correzione r/12 in main.js snack4/ termine esercizi

--- assets/ciclo for snak4/js/main.js
+++ assets/ciclo for snak4/js/main.js
@@ -9,9 +9,8 @@ for(let i = 0; i < 5; i++){
     if(utente % 2 === 0 ){
         console.log("il numero che hai inserito e pari")
 
-    }else if(utente % 2 === 1 ){
-        console.log("il numero che hai inserito e dispari")
     }else{
-        console.log(utente % 2 === 1)
+    
+        console.log("Il numero che hai inserito è dispari. Il successivo è: " + (utente + 1))
     }
 }
